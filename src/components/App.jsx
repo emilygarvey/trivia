7420ea5c21a8cf332d1f2ba1a6b5483f069f08a3
Emily Garvey <41160828+emilygarvey@users.noreply.github.com>
2020-12-03 11:21:35 -0500

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import '../css/App.css';
-import sampleData from '../sample_data.json';
-import Question from './Question.jsx';
+import React, { Component } from "react";
+import "../css/App.css";
+import sampleData from "../sample_data.json";
+import Question from "./Question.jsx";
 // import components
 
+// 19-20 instructions
 class App extends Component {
-
   render() {
     return (
       <div className="app">
